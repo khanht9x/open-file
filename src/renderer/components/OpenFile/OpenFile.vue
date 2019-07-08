941163934e7dca21bafa55f58b9bd7bf324945ee
khanht <khanh.t@citigo.com.vn>
2019-07-08 13:40:44 +0700

    update

--- src/renderer/components/OpenFile/OpenFile.vue
+++ src/renderer/components/OpenFile/OpenFile.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <p>Hello Word</p>
   </div>
 </template>
 
@@ -14,9 +14,7 @@ export default {
   },
   mounted () {
     const path = __static + '/yunxi-excel.xls'
-    console.log(path)
     this.open(path)
-    this.open('/home/khanht/Desktop/code/code/electron_open_file/static/yunxi-excel.xls')
   }
 }
 </script>
